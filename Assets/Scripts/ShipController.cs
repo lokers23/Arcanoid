72e9ecd4004c97f3fb3cd317a3cded263dc0a0a2
Vlad <pevloks@gmail.com>
2022-08-02 16:14:29 +0300

    Added a code so that the ship does not go abroad

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,16 +7,21 @@ using UnityEngine.EventSystems;
 public class ShipController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidbodyComponent;
-    [SerializeField] private float speed = 15f;
     [SerializeField] private Camera currentCamera;
+    [SerializeField] private SpriteRenderer sprite;
     
+    [SerializeField] private float speed = 5f;
     private float _stepX = 0f;
     private bool _mousePressed;
+    private float border;
     private Vector2 _startPosition ;
 
     private void Awake()
     {
         _startPosition = currentCamera.transform.position;
+        var leftBottomCorner = currentCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        var rightBottomCorner = currentCamera.ViewportToWorldPoint(new Vector2(1, 0));
+        border = Vector2.Distance(leftBottomCorner, rightBottomCorner) / 2;
     }
 
     private void FixedUpdate()
@@ -48,6 +53,9 @@ public class ShipController : MonoBehaviour
         var worldPosition = currentCamera.ScreenToWorldPoint(Input.mousePosition);
         _stepX =  worldPosition.x > _startPosition.x ? 1f : -1f;
         var position = rigidbodyComponent.position.x + _stepX * speed * Time.fixedDeltaTime;
+        
+        var size = sprite.size.x / 2;
+        position = Mathf.Clamp(position, -border + size, border - size);
         rigidbodyComponent.MovePosition(new Vector2(position, rigidbodyComponent.position.y));
     }
     
