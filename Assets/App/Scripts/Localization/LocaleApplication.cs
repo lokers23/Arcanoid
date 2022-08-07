a5805ea5a7c18011a834e939bf4d9da3bf0629df
Vlad <pevloks@gmail.com>
2022-08-08 00:51:47 +0300

    Changed scripts

--- Assets/App/Scripts/Localization/LocaleApplication.cs
+++
@@ -1,8 +0,0 @@
-namespace App.Scripts.Localization
-{
-    public class LocaleApplication
-    {
-        public const string EN = "EN";
-        public const string RU = "RU";
-    }
-}
--- Assets/App/Scripts/Localization/LocalizationManager.cs
+++ Assets/App/Scripts/Localization/LocalizationManager.cs
@@ -1,135 +1,51 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 using UnityEngine;
-using Path = System.IO.Path;
 
 namespace App.Scripts.Localization
 {
-    public class LocalizationManager : MonoBehaviour
+    public static class LocalizationManager
     {
-        private const string FileNamePrefix = "text_";
-        private const string FileExtension = ".json";
-        private string _fullNameTextFile;
-        private string _fullPathTextFile;
-        private string _languageChoose = "RU";
-        private string _loadedJsonText = "";
-
-        [HideInInspector]
-        public bool _isReady;
-        private bool _isFileFound;
-        private bool _isTryChangeLanguageRunTime;
-
-        private Dictionary<string, string> _localizeDictionary;
-        private LocalizationData _loadedData;
-
-        private static LocalizationManager LocalizationManagerInstance;
-
-        public static LocalizationManager Instance
-        {
-            get
-            {
-                if (LocalizationManagerInstance == null)
-                {
-                    LocalizationManagerInstance = FindObjectOfType(typeof(LocalizationManager)) as LocalizationManager;
-                }
-
-                return LocalizationManagerInstance;
-            }
-        }
-
-        private IEnumerator Start()
-        {
-            _languageChoose = LocaleHelper.GetSupportedLanguageCode();
-            _fullNameTextFile = $"{FileNamePrefix}{_languageChoose.ToLower()}{FileExtension}";
-
-            _fullPathTextFile = Path.Combine(Application.streamingAssetsPath, _fullNameTextFile);
-
-            yield return StartCoroutine(LoadJsonLanguageData());
-            _isReady = true;
-        }
-
-        private IEnumerator LoadJsonLanguageData()
-        {
-            CheckFileExist();
+        public static event Action OnChangeLanguage;
         
-            yield return new WaitUntil(() => _isFileFound);
+        private const string KeyPref = "Localization";
+        private const string Folder = "Localization";
+        private const SystemLanguage DefaultLanguage = SystemLanguage.Russian;
         
-            _loadedData = JsonUtility.FromJson<LocalizationData>(_loadedJsonText);
-            _localizeDictionary = new Dictionary<string, string>(_loadedData.items.Count);
-            _loadedData.items.ForEach(item =>
-            {
-                _localizeDictionary.Add(item.key, item.value);
-            });
-            yield return null;
-        }
+        private static string _fullNameTextFile;
+        private static string _fullPathTextFile;
 
-        private void CheckFileExist()
+        private static Dictionary<string, string> _localizeDictionary;
+
+        static LocalizationManager()
         {
-            if (!File.Exists(_fullPathTextFile))
+            if (!PlayerPrefs.HasKey(KeyPref))
             {
-                CopyFileFromRessource();
+                PlayerPrefs.SetInt(KeyPref, (int)DefaultLanguage);
+                LoadJsonFile((SystemLanguage)PlayerPrefs.GetInt(KeyPref));
             }
             else
             {
-                LoadFileContents();
+                LoadJsonFile((SystemLanguage)PlayerPrefs.GetInt(KeyPref));
             }
         }
 
-        private void LoadFileContents()
+        private static void LoadJsonFile(SystemLanguage language)
         {
-            _loadedJsonText = File.ReadAllText(_fullPathTextFile);
-            _isFileFound = true;
+            _fullNameTextFile = Enum.GetName(typeof(SystemLanguage),language);
+            _fullPathTextFile = $"{Folder}/{_fullNameTextFile}";
+            var textAsset = Resources.Load<TextAsset>(_fullPathTextFile);
+            if (textAsset == null)
+            {
+                throw new ArgumentNullException(nameof(_fullPathTextFile), "Localization file is missing");
+            }
+                
+            _localizeDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(textAsset.ToString());
         }
-        private void CopyFileFromRessource()
-        {
-            var myFile = Resources.Load($"{FileNamePrefix}{_languageChoose}") as TextAsset;
-            if (myFile == null)
-            {
-                return;
-            }
-
-            _loadedJsonText = myFile.ToString();
-            File.WriteAllText(_fullPathTextFile, _loadedJsonText);
-            StartCoroutine(WaitCreationFile());
-        }
-
-        private IEnumerator WaitCreationFile()
-        {
-            FileInfo myFile = new FileInfo(_fullNameTextFile);
-            float timeOut = 0.0f;
-            while (timeOut < 5.0f && !IsFileFinishCreate(myFile))
-            {
-                timeOut += Time.deltaTime;
-                yield return null;
-            }
-        }
-
-        private bool IsFileFinishCreate(FileInfo fileInfo)
-        {
-            FileStream stream = null;
-            try
-            {
-                stream = fileInfo.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-            }
-            catch (IOException)
-            {
-                _isFileFound = true;
-                return true;
-            }
-            finally
-            {
-                if (stream != null)
-                {
-                    stream.Close();
-                }
-            }
-
-            _isFileFound = false;
-            return false;
-        }
-
-        public string GetTextForKey(string localizationKey)
+        
+        public static string GetTextByKey(string localizationKey)
         {
             if (_localizeDictionary.ContainsKey(localizationKey))
             {
@@ -139,35 +55,30 @@ namespace App.Scripts.Localization
             return "";
         }
         
-        private IEnumerator SwitchLanguage(string language)
+        public static void ChangeLanguage(SystemLanguage language)
         {
-            if (!_isTryChangeLanguageRunTime)
-            {
-                _isTryChangeLanguageRunTime = true;
-                _isFileFound = false;
-                _isReady = false;
-                _languageChoose = language;
-
-                _fullNameTextFile = $"{FileNamePrefix}{_languageChoose.ToLower()}{FileExtension}";
-                _fullPathTextFile = Path.Combine(Application.streamingAssetsPath, _fullNameTextFile);
-
-                yield return StartCoroutine(LoadJsonLanguageData());
-                _isReady = true;
-
-                LocalizedText[] arrayText = FindObjectsOfType<LocalizedText>();
-
-                for (int i = 0; i < arrayText.Length; i++)
-                {
-                    arrayText[i].Attributiontext();
-                }
-
-                _isTryChangeLanguageRunTime = false;
-            }
+            LoadJsonFile(language);
+            PlayerPrefs.SetInt(KeyPref, (int)language);
+            OnChangeLanguage?.Invoke();
         }
 
-        public void ChangeLanguage(string language)
+        public static List<SystemLanguage> GetLanguages()
         {
-            StartCoroutine(SwitchLanguage(language));
+            var allFiles = Resources.LoadAll<TextAsset>(Folder).ToList();
+            if (allFiles.Count == 0)
+            {
+                throw new ArgumentException("Localization files are missing");
+            }
+            
+            var languages = new List<SystemLanguage>(allFiles.Count);
+            foreach (var file in allFiles)
+            {
+                var name = file.name;
+                Enum.TryParse<SystemLanguage>(name, out var language);
+                languages.Add(language);
+            }
+            
+            return languages;
         }
     }
 }
