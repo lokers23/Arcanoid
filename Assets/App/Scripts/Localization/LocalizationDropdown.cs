5c035fa4f1e3b1d8f14559f56b67c6b2319789d3
Vlad <pevloks@gmail.com>
2022-08-08 00:49:15 +0300

    Fixed localization

---
+++ Assets/App/Scripts/Localization/LocalizationDropdown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace App.Scripts.Localization
+{
+    public class LocalizationDropdown : MonoBehaviour
+    {
+        [SerializeField] private Dropdown dropdown;
+
+        private void Start()
+        {
+            var languages = LocalizationManager.GetLanguages();
+            var names = new List<string>();
+            foreach (var language in languages)
+            {
+                var languageString = Enum.GetName(typeof(SystemLanguage), language);
+                names.Add(languageString);
+            }
+            
+            dropdown.AddOptions(names);
+        }
+
+        public void ChangeValue()
+        {
+            var value = dropdown.value;
+            var selectedString = dropdown.options[value].text;
+            var tryParse = Enum.TryParse<SystemLanguage>(selectedString, out var language);
+            if (tryParse)
+            {
+                LocalizationManager.ChangeLanguage(language);
+            }
+        }
+    }
+}
---
+++ Assets/App/Scripts/Localization/LocatizedText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace App.Scripts.Localization
+{
+    public class LocatizedText : MonoBehaviour
+    {
+        [SerializeField] private string localizationKey;
+        [SerializeField] private TextMeshProUGUI textComponent;
+        
+        private void Start()
+        {
+            SetText();
+        }
+
+        private void OnEnable()
+        {
+            LocalizationManager.OnChangeLanguage += SetText;
+        }
+
+        private void OnDisable()
+        {
+            LocalizationManager.OnChangeLanguage -= SetText;
+        }
+
+        private void SetText()
+        {
+            textComponent.text = LocalizationManager.GetTextByKey(localizationKey).ToUpper();
+        }
+    }
+}
