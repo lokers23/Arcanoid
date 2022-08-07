39de03f313c03f1dd3edc4e0aab17396dc7d66e4
Vlad <pevloks@gmail.com>
2022-08-08 00:53:24 +0300

    Created popup manager

---
+++ Assets/App/Scripts/Entities/PopUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace App.Scripts.Entities
+{
+    [Serializable]
+    public class PopUp
+    {
+        public string popUpId;
+        public GameObject popUpPrefab;
+    }
+}
---
+++ Assets/App/Scripts/PopUp/PopUpManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Scripts.PopUp
+{
+    public class PopUpManager : MonoBehaviour
+    {
+        private List<Entities.PopUp> Popups = new List<Entities.PopUp>();
+
+        private ObjectPool _objectPool;
+        public static PopUpManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            _objectPool = ObjectPool.Instance;
+        }
+        
+
+        public void ShowPopUp(string popUpId)
+        {
+            GameObject popup = Instance._objectPool.GetObjectFromPool(popUpId);
+            if (popup != null)
+            {
+                Instance.Popups.Add(new Entities.PopUp
+                {
+                    popUpId = popUpId,
+                    popUpPrefab = popup
+                });
+            }
+        }
+
+        public void HidePopUp()
+        {
+            var popup = Instance.Popups[Instance.Popups.Count - 1];
+            Instance.Popups.Remove(popup);
+            Instance._objectPool.SetObjectToPool(popup.popUpPrefab);
+        }
+    }
+}
